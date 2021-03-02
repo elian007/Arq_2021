29305a22f06fa535fbe97732c684e32818ac14f2
Elian07 <elians.2016@alunos.utfpr.edu.br>
2021-03-02 19:31:33 -0300

    Criando projeto e metodos para calculos

---
+++ Arquitetura_Projeto/src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+import Model.Solo;
+
+/**
+ *
+ * @author elian
+ */
+public class Main {
+    Solo solo = new Solo();
+    
+    solo.calculoFosforo();
+    
+}
---
+++ Arquitetura_Projeto/src/main/java/Model/Solo.java
@@ -0,0 +1,47 @@
+
+package Model;
+
+/**
+ *
+ * @author elian
+ */
+public class Solo {
+    
+    public double calculoFosforo(double solo, double ideal){
+        double resultado = 0;
+        
+        return resultado;
+    }
+    
+     public double calculoPotassio(double solo, double ideal){
+        double resultado = 0;
+        
+        return resultado;
+    }
+     
+      public double calculoCalcio(double solo, double ideal){
+        double resultado = 0;
+        
+        return resultado;
+    }
+       public double calculomagnesio(double solo, double ideal){
+        double resultado = 0;
+        
+        return resultado;
+    }
+        public double calculoEnxofre(double solo, double ideal){
+        double resultado = 0;
+        
+        return resultado;
+    }
+         public double calculoAluminio(double solo, double ideal){
+        double resultado = 0;
+        
+        return resultado;
+    }
+          public double calculoHmaisAL(double solo, double ideal){
+        double resultado = 0;
+        
+        return resultado;
+    }
+}
